2dcce8b25659ee7e7b4b0e413a6824d7f0f514d2
xennia316 <xennia316@gmail.com>
2023-03-23 06:05:44 +0100

    feat: regular number button

---
+++ app/src/components/buttons/normalButton.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default NormalButton = () => {
+    return ( <div className="h-screen w-screen bg-red-400">
+                <button className="h-screen w-screen bg-black ">
+                    Hiii I'm here
+                </button>
+        </div>)
+
+}
