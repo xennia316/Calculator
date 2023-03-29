025966be5bb7e2c433c9ee38b60f24c05d4beded
xennia316 <xennia316@gmail.com>
2023-03-29 14:07:56 +0100

    feat: control center

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import NormalButton from "./normalButton.component";
+import OperationButton from "./operationButton.component";
+import FunctionButton from "./functionButton.component";
+import Screen from "./screen.component";
+
+export {NormalButton, OperationButton, FunctionButton, Screen}
