83fe412e7932aef102e9bc70b035bdc2952a859b
xennia316 <xennia316@gmail.com>
2023-03-29 14:08:39 +0100

    feat: screen mount

---
+++ client/src/components/screen.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Screen = ({ answer, calculation }) => {
+	return (
+		<div className="w-full h-24 flex flex-col bg-orange-900 text-white  items-end text-right text-4xl rounded-t-xl">
+			<div className="h-1/2 text-right">{calculation ? calculation : 0}</div>
+			<div className="h-1/2 text-right font-bold">{answer ? answer : 0}</div>
+		</div>
+	);
+};
+
+export default Screen;
