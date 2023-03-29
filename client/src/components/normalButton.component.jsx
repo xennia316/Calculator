d1f93a31e768f7e5cc5c18d2ea7ee47d9e766874
xennia316 <xennia316@gmail.com>
2023-03-29 14:08:10 +0100

    feat: Normal button mount

--- client/src/components/normalButton.component.jsx
+++ client/src/components/normalButton.component.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const NormalButton = ({numberValue}) => {
-  return (
-    <button className='w-10 h-10 bg-amber-800 text-white text-center'>
-        {numberValue}
-    </button>
-  )
-}
+const NormalButton = ({ numberValue }) => {
+	return (
+		<button className="grow h-14 bg-orange-900 text-white text-center backdrop-opacity-30 ">
+			{numberValue}
+		</button>
+	);
+};
 
-export default NormalButton
+export default NormalButton;
