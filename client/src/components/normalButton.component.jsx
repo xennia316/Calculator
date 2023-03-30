d51c6f8393023fa981ab2470c3f878efa8ab9aff
xennia316 <xennia316@gmail.com>
2023-03-30 08:52:36 +0100

    iteration

--- client/src/components/normalButton.component.jsx
+++ client/src/components/normalButton.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-const NormalButton = ({ numberValue }) => {
+const NormalButton = ({ numberValue, changeState }) => {
+	const handleClick = () => {
+		changeState(numberValue);
+	};
 	return (
-		<button className="grow h-14 bg-orange-900 text-white text-center backdrop-opacity-30 ">
+		<button
+			className="grow h-14 bg-orange-900 text-white text-center backdrop-opacity-30 "
+			onClick={handleClick}
+		>
 			{numberValue}
 		</button>
 	);
