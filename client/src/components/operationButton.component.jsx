723d8832e613a0aa2db2ac897e540f2b1a33aa7c
xennia316 <xennia316@gmail.com>
2023-03-30 08:53:21 +0100

    iteration on button

--- client/src/components/operationButton.component.jsx
+++ client/src/components/operationButton.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-const OperationButton = ({ operation }) => {
+const OperationButton = ({ operation, changeState }) => {
+	const handleClick = () => {
+		changeState(operation);
+	};
 	return (
-		<button className="h-14 grow bg-yellow-600 text-white text-center bg-opacity-90">
+		<button
+			className="h-14 grow bg-yellow-600 text-white text-center bg-opacity-90"
+			onClick={handleClick}
+		>
 			{operation}
 		</button>
 	);
