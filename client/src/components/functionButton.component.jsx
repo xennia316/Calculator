9c0405d72d65000da033331435ea115f49d405a4
xennia316 <xennia316@gmail.com>
2023-03-30 08:52:15 +0100

    progress

--- client/src/components/functionButton.component.jsx
+++ client/src/components/functionButton.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
-const FunctionButton = ({ func }) => {
+const FunctionButton = ({ func, changeState }) => {
+	const handleClick = () => {
+		changeState(func);
+	};
 	return (
-		<button className=" h-14 bg-orange-900 text-white text-center backdrop-opacity-10">
+		<button
+			className=" h-14 bg-orange-900 text-white text-center backdrop-opacity-10"
+			onClick={handleClick}
+		>
 			{func}
 		</button>
 	);
