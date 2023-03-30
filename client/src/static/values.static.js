5830326fd898e1128921e0cead8ad3bfa72366ff
xennia316 <xennia316@gmail.com>
2023-03-30 08:53:41 +0100

    iteration on button

---
+++ client/src/static/values.static.js
@@ -0,0 +1,24 @@
+const Values = [
+	{ type: "function", actValue: "C" },
+	{ type: "function", actValue: "+/-" },
+	{ type: "function", actValue: "%" },
+	{ type: "operation", actValue: "/" },
+	{ type: "number", actValue: "7" },
+	{ type: "number", actValue: "8" },
+	{ type: "number", actValue: "9" },
+	{ type: "operation", actValue: "*" },
+	{ type: "number", actValue: "4" },
+	{ type: "number", actValue: "5" },
+	{ type: "number", actValue: "6" },
+	{ type: "operation", actValue: "-" },
+	{ type: "number", actValue: "1" },
+	{ type: "number", actValue: "2" },
+	{ type: "number", actValue: "3" },
+	{ type: "operation", actValue: "+" },
+	{ type: "number", actValue: "0" },
+	{ type: "number", actValue: "." },
+	{ type: "function", actValue: "e" },
+	{ type: "operation", actValue: "=" },
+];
+
+export default Values;
