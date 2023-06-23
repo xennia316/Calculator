7d49f076bf76dbbc3549743dfe28bb75975b56c0
xennia316 <xennia316@gmail.com>
2023-06-23 23:48:24 +0100

    handle click

--- client/src/components/operationButton.component.jsx
+++ client/src/components/operationButton.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-const OperationButton = ({ operation, setCalc }) => {
+const OperationButton = ({ operation, valueArray }) => {
+	const handleClick = () => {
+		valueArray.push(operation);
+		console.log("Newly changed", valueArray);
+	};
 	return (
 		<button
 			className="h-14 grow bg-yellow-600 text-white text-center bg-opacity-90"
-			onClick={() => {
-				setCalc();
-			}}
+			onClick={handleClick}
 		>
 			{operation}
 		</button>
