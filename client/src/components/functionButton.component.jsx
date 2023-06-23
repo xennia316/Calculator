ee1560407df961d8fd5e1c147b3f44e5366d9096
xennia316 <xennia316@gmail.com>
2023-06-23 23:47:54 +0100

    on click function review

--- client/src/components/functionButton.component.jsx
+++ client/src/components/functionButton.component.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-const FunctionButton = ({ func, setCalc }) => {
+const FunctionButton = ({ func, valueArray, setAnswer }) => {
+	const handleClick = () => {
+		if (func === "C") {
+			valueArray.length = 0;
+			console.log("Happening", valueArray);
+		} else if (func === "+/-") {
+			console.log("Shifting here", func);
+			if (valueArray[0] === "-") {
+				valueArray.shift();
+			} else {
+				valueArray.unshift("-");
+				console.log("Shifting here", func);
+			}
+		} else if (func === "=") {
+			let pro = valueArray.join("");
+			console.log("Pro", pro, typeof pro);
+			setAnswer(pro);
+		} else if (func === "e" || "%") {
+			valueArray.push(func);
+		}
+	};
+
 	return (
 		<button
 			className=" h-14 bg-orange-900 text-white text-center backdrop-opacity-10"
-			onClick={() => {
-				setCalc = { func };
-			}}
+			onClick={handleClick}
 		>
 			{func}
 		</button>
